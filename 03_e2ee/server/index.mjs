34d38889523cb5570e979a02841904700893d0de
Olivia Tobin <z5406144@unsw.edu.au>
2023-11-02 20:53:59 +1100

    implement ee2e actual

---
+++ 03_e2ee/server/index.mjs
@@ -0,0 +1,55 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 3000 })
+
+let left
+let right
+
+let leftPublicKey
+let rightPublicKey
+
+wss.on("connection", (ws) => {
+  let getOther
+  if (left === undefined) {
+    left = ws
+    getOther = () => right
+  } else if (right === undefined) {
+    right = ws
+    getOther = () => left
+  } else {
+    ws.send("chat room full")
+    return
+  }
+
+  ws.on("message", (data) => {
+    const message = data.toString()
+    const other = getOther()
+
+    // send the clients' public keys to the other client
+    if (message.charAt(0) === "#") {
+      if (other === left) {
+        rightPublicKey = message.substring(1)
+      } else {
+        leftPublicKey = message.substring(1)
+      }
+      if (other !== undefined) {
+        if (other === left) {
+          ws.send("#" + leftPublicKey)
+          other.send("#" + rightPublicKey)
+        } else if (other === right) {
+          ws.send("#" + rightPublicKey)
+          other.send("#" + leftPublicKey)
+        }
+      }
+    } else {
+      if (other === undefined) {
+        ws.send("chat partner not connected")
+        return
+      } 
+
+      console.log(message)
+
+      other.send(message)
+    }
+  })
+})
